9099a868c1e94172d304014ce37cc457da53da25
Gonzalo Ramos <gnzramos@gmail.com>
2022-09-02 21:47:38 -0500

    Fixes and adding ingredientes

--- src/js/index.js
+++ src/js/index.js
@@ -13,21 +13,41 @@ function randomFunction() {
   fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${randomCharacter}`)
   .then((response) => response.json())
     .then((data) => {
-      let html = "";
+      listaDeComida.innerHTML = ""
       if (data.meals) {
         data.meals.forEach((comida) => {
-          html += `
-                <div class="comida-articulo" data-id = "${comida.idMeal}">
-                    <div class="comida-imagen">
-                        <img src="${comida.strMealThumb}" alt="Imagen de la receta" class="comida-boton">
-                    </div>
-                    <div class="comida-nombre">
-                        <h3>${comida.strMeal}</h3>
-                    </div>
-                </div>
-                `;
+
+          // Creamos toda la card
+      const cardFragment = document.createDocumentFragment();
+      let comidaArticulo = cardFragment
+        .appendChild(document.createElement('div'))
+      comidaArticulo.className = ('comida-articulo')
+      comidaArticulo.dataset.id = comida.idMeal
+
+      // Div de la imagen
+      let contenedorImagen = comidaArticulo
+        .appendChild(document.createElement('div'))
+      contenedorImagen.className = ('comida-imagen')
+
+      let imagenComida = contenedorImagen
+        .appendChild(document.createElement('img'))
+        imagenComida.src = comida.strMealThumb
+        imagenComida.alt = ('Imagen de la receta')
+        imagenComida.className = ('comida-boton')
+      
+      // Div del nombre de la receta
+      let contenedorComida = comidaArticulo
+        .appendChild(document.createElement('div'))
+        contenedorComida.className = ('comida-nombre')
+
+      let nombreComida = contenedorComida
+        .appendChild(document.createElement('h3'))
+        nombreComida.textContent = comida.strMeal
+        
+        listaDeComida.append(comidaArticulo)
+
         })};
-        listaDeComida.innerHTML = html;
+        /* listaDeComida.innerHTML = html; */
         botonVolver.style.display = 'none'
         listaDeComida.classList.remove('sinCoincidencia')
         botonVolver.href = '../index.html'
@@ -55,31 +75,49 @@ function consulta() {
   fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${busqueda}`)
     .then((response) => response.json())
     .then((data) => {
-      let html = "";
+      listaDeComida.innerHTML = ""
       if (data.meals) {
         data.meals.forEach((comida) => {
-          html += `
-          <div class="comida-articulo" data-id = "${comida.idMeal}">
-                    <div class="comida-imagen">
-                      <img src="${comida.strMealThumb}" alt="Imagen de la receta" class="comida-boton">
-                    </div>
-                    <div class="comida-nombre">
-                        <h3>${comida.strMeal}</h3>
-                        </div>
-                        </div>
-                        `;
+      
+      // Creamos toda la card
+      const cardFragment = document.createDocumentFragment();
+      let comidaArticulo = cardFragment
+        .appendChild(document.createElement('div'))
+      comidaArticulo.className = ('comida-articulo')
+      comidaArticulo.dataset.id = comida.idMeal
+
+      // Div de la imagen
+      let contenedorImagen = comidaArticulo
+        .appendChild(document.createElement('div'))
+      contenedorImagen.className = ('comida-imagen')
+
+      let imagenComida = contenedorImagen
+        .appendChild(document.createElement('img'))
+        imagenComida.src = comida.strMealThumb
+        imagenComida.alt = ('Imagen de la receta')
+        imagenComida.className = ('comida-boton')
+      
+      // Div del nombre de la receta
+      let contenedorComida = comidaArticulo
+        .appendChild(document.createElement('div'))
+        contenedorComida.className = ('comida-nombre')
+
+      let nombreComida = contenedorComida
+        .appendChild(document.createElement('h3'))
+        nombreComida.textContent = comida.strMeal
+        
+        listaDeComida.append(comidaArticulo)
+        listaDeComida.classList.remove('sinCoincidencia')
                       });
                       // En caso de no encontrar una receta
                     } else {
-                      html = "No se han encontrado recetas con ese nombre";
-                      listaDeComida.classList.add("sinCoincidencia");
+                      listaDeComida.innerHTML = "No se han encontrado recetas con ese nombre"
+                      listaDeComida.classList.add("sinCoincidencia")
         }
-          listaDeComida.innerHTML = html;
           botonVolver.style.display = 'none'
       });
     }
   }
-
 
 // Funcion para obtener el id de la receta
 listaDeComida.addEventListener("click", obtenerId);
@@ -87,70 +125,92 @@ function obtenerId(e) {
   e.preventDefault();
   if (e.target.classList.contains("comida-boton")) {
     let articulo = e.target.parentElement.parentElement;
-    console.log(articulo)
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${articulo.dataset.id}`)
     .then((response) => response.json())
     .then((data) => {
-      let html1 = '', html2 = ''
       listaDeComida.innerHTML = ''
       data.meals.forEach((comida) => {
-        html1 += `
-        <div class="comida-articulo" data-id = "${comida.idMeal}">
-                  <div class="comida-imagen">
-                      <img src="${comida.strMealThumb}" alt="Imagen de la receta">
-                  </div>
-                  <div class="comida-nombre">
-                    <h3>${comida.strMeal}</h3>
-                  </div>
-                </div>
-                <div class="comida-ingredientes">
-                `
-            let obj = Object.keys(comida)
-            console.log(obj)
-            // for (let i = 0; i < comida.; i++) {
-            //     ingre = `comida.strIngredient${i}`
-            //     console.log(ingre)
-            // }
-            html2 += `
-            <h5>Ingredients</h5>
-            <p>${comida.strIngredient1}, ${comida.strMeasure1}</p>
-            </div>
-            <div class="comida-preparacion">
-            <h5>Instructions</h5>
-            <p>${comida.strInstructions}</p>
-            </div>
-            `;
-          })
-          botonVolver.style.display = 'block'
-        listaDeComida.innerHTML = html1 + html2;
+      
+      // Creamos toda la card
+      const cardFragment = document.createDocumentFragment();
+      let comidaArticulo = cardFragment
+        .appendChild(document.createElement('div'))
+      comidaArticulo.className = ('comida-articulo')
+      comidaArticulo.dataset.id = articulo.dataset.id
+      
+
+      // Div de la imagen
+      let contenedorImagen = comidaArticulo
+        .appendChild(document.createElement('div'))
+      contenedorImagen.className = ('comida-imagen')
+
+      let imagenComida = contenedorImagen
+        .appendChild(document.createElement('img'))
+        imagenComida.src = comida.strMealThumb
+        imagenComida.alt = ('Imagen de la receta')
+      
+      // Div del nombre de la receta
+      let contenedorComida = comidaArticulo
+        .appendChild(document.createElement('div'))
+        contenedorComida.className = ('comida-nombre')
+
+      let nombreComida = contenedorComida
+        .appendChild(document.createElement('h3'))
+        nombreComida.textContent = comida.strMeal
+      
+      // Div de los ingredientes
+      let comidaIngredientes = cardFragment
+        .appendChild(document.createElement('div'))
+        comidaIngredientes.className = ('comida-articulo')
+      
+      let nombreIngredientes = comidaIngredientes
+        .appendChild(document.createElement('h5'))
+        nombreIngredientes.textContent = ('Ingredients')
+        nombreIngredientes.className = ('comida-nombre')
+
+        
+        //Conviriendo los contenidos de Json en un array
+        mealArray = Object.values(data.meals[0]);
+        var ingredientsList = [];
+        var measuresList = [];
+        //Obteniendo los ingredientes en un array
+        for (let i = 9; i <= 28; i++) {
+          if (!mealArray[i]) break;
+          ingredientsList.push(mealArray[i]);
+        }
+        //Obteniendo las medidas en un array
+        for (let i = 29; i <= 48; i++) {
+          if (mealArray[i] === "" || mealArray[i] === " ") break;
+          measuresList.push(mealArray[i]);
+        }
+        
+        for (let c = 0; c < ingredientsList.length; c++) {
+          listaIng = `${ingredientsList[c]}, ${measuresList[c]}`
+        
+        let ingredientesUnoPorUno = comidaIngredientes
+          .appendChild(document.createElement('p'))
+          ingredientesUnoPorUno.textContent = listaIng
+        }
+
+        // Div de la preparación
+      let comidaPreparacion = cardFragment
+        .appendChild(document.createElement('div'))
+        comidaPreparacion.className = ('comida-articulo')
+
+      let nombrePreparacion = comidaPreparacion
+        .appendChild(document.createElement('h5'))
+        nombrePreparacion.textContent = ('Instructions')
+        nombrePreparacion.className = ('comida-nombre')
+
+      let recetaPreparacion = comidaPreparacion
+        .appendChild(document.createElement('p'))
+        recetaPreparacion.textContent = comida.strInstructions
+        recetaPreparacion.className = ('comida-nombre')
+
+      listaDeComida.append(comidaArticulo, comidaIngredientes, comidaPreparacion)
+        
+      botonVolver.style.display = 'block'
+      })
       });
   }
 }
-
-
-// Función deprecada //
-
-// function aleatorio() {
-//   fetch("https://www.themealdb.com/api/json/v1/1/random.php")
-//   .then((response) => response.json())
-//   .then((data) => {
-//     let html = "";
-//       if (data.meals) {
-//         data.meals.forEach((comida) => {
-//           html += `
-//             <div class="comida-articulo" data-id = "${comida.idMeal}">
-//                 <div class="comida-imagen">
-//                     <img src="${comida.strMealThumb}" alt="Imagen de la receta">
-//                 </div>
-//                 <div class="comida-nombre">
-//                     <h3>${comida.strMeal}</h3>
-//                     <a href="#" class="comida-boton">Ver receta</a>
-//                 </div>
-//             </div>
-//             `;
-//       });
-//       }
-//       listaDeComida.innerHTML = html;
-//       // listaDeComida.appendChild(html) 
-//     });
-//   }
